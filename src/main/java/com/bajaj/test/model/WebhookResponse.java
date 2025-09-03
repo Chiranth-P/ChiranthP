368bdcd270297fbf87caa0560f9c5570019569cb
Chiranth P <131045938+Chiranth-P@users.noreply.github.com>
2025-09-04 01:13:34 +0530

    Update WebhookResponse.java

--- src/main/java/com/bajaj/test/model/WebhookResponse.java
+++ src/main/java/com/bajaj/test/model/WebhookResponse.java
@@ -1,4 +1,14 @@
 package com.bajaj.test.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
 public class WebhookResponse {
+   
+    @JsonProperty("webhook_url")
+    private String webhookUrl;
+
+    @JsonProperty("access_token")
+    private String accessToken;
 }
