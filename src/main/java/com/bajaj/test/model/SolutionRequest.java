a18cca5f5852184459bf105d5eb01e5200288dde
Chiranth P <131045938+Chiranth-P@users.noreply.github.com>
2025-09-04 01:12:12 +0530

    Update SolutionRequest.java

--- src/main/java/com/bajaj/test/model/SolutionRequest.java
+++ src/main/java/com/bajaj/test/model/SolutionRequest.java
@@ -1,4 +1,10 @@
 package com.bajaj.test.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class SolutionRequest {
+    private String finalQuery;
 }
